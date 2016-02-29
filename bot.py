8aee2da369d4927d7c8523331fc8aca2877d2f83
njzk2 <masperos+github@gmail.com>
2016-02-28 21:01:27 -0500

    Clean the final move choice

--- bot.py
+++ bot.py
@@ -25,6 +25,7 @@ def select_move(my_discs, op_discs, op_move, time, settings):
     op = [2,1][me - 1]
 
     moves = []
+    value = evaluation.LOSE
     for disc, new_discs in utils.nodes(my_discs, op_discs, settings['field_columns'], settings['field_rows']):
         current_value = evaluation.scan(new_discs, disc)
         if current_value == evaluation.WIN:
@@ -32,32 +33,30 @@ def select_move(my_discs, op_discs, op_move, time, settings):
             break
         move = minimax(new_discs, op_discs, depth - 1, False, settings['field_columns'], settings['field_rows'])
         move[1].append(disc)
+        if not moves or move[0] > moves[0][0]:
+            value = move[0]
+            moves = []
         moves.append(move)
-    moves.sort(key=lambda move: -len(move[1]))
-    moves.sort(key=lambda move: -move[0])
     if not moves or not moves[0] or not moves[0][1]:
         # Nothing of worth was found (we probably lost)
         return next(utils.nodes(my_discs, op_discs))[0][0]
-    move = moves[0]
     if log:
-        print 'minimax result', move
+        print 'minimax result'
         for _move in moves:
             print _move
 
-    if move[0] == evaluation.LOSE:
-        # All moves are losing, take the one that takes the longest
+    if value == evaluation.LOSE:
+        # Those moves are losing, take the one that takes the longest
         moves.sort(key=lambda move: -len(move[1]))
-        move = moves[0]
-
-    if move[0] == 0:
+    elif value == evaluation.WIN:
+        # Those moves are winning, take the shortest
+        moves.sort(key=lambda move: len(move[1]))
+    elif value == 0:
         # not losing, but no winning move found, playing close to center
-        moves = [mv[-1] for score, mv in moves if score != evaluation.LOSE]
-        moves.sort(key=lambda (x, y): abs(x - settings['field_columns'] // 2) - y)
-        if log:
-            print moves
-        return moves[0][0]
-    else:
-        return move[1][-1][0]
+        moves.sort(key=lambda move: abs(move[1][-1][0] - settings['field_columns'] // 2) - move[1][-1][1])
+
+    move = moves[0]
+    return move[1][-1][0]
 
 if __name__ == '__main__':
     while True:
